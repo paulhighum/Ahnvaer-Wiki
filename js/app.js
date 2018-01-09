95ef8cd37b08f24d80a8e93e64090bcb2bdef77b
Paul Highum <phigh07@gmail.com>
2018-01-09 10:23:35 -0700

    updates js styling

--- js/app.js
+++ js/app.js
@@ -12,43 +12,69 @@ fetch(APIurl)
     return resp
   })
 
-function addRaceInfoToPage (raceArray) {
+function addRaceInfoToPage(raceArray) {
   raceArray.forEach(race => {
-    if(race.name === pageRace) {
+    if (race.name === pageRace) {
       pageRaceDescription.textContent = race.description
       let regionSentenceStart = `${race.name}s primarily live in the `
       let citiesSentenceStart = ""
       switch (race.geography.regions.length) {
-        case 1:
-          pageRaceRegions.textContent = `${regionSentenceStart} ${race.geography.regions[0]} region.`
-          citiesSentenceStart = "Within this region they make up a large percentage of the population in the "
-          break
-        case 2:
-          pageRaceRegions.textContent = `${regionSentenceStart} ${race.geography.regions[0]} and ${race.geography.regions[1]} regions.`
-          citiesSentenceStart = "Within these regions they make up a large percentage of the population in the "
-          break
-        case 3:
-          pageRaceRegions.textContent = `${regionSentenceStart} ${race.geography.regions[0]}, ${race.geography.regions[1]}, and ${race.geography.regions[2]} regions.`
-          citiesSentenceStart = "Within these regions they make up a large percentage of the population in the "
-        default:
+      case 1:
+        pageRaceRegions.textContent = `${regionSentenceStart} ${
+          race.geography.regions[0]
+        } region.`
+        citiesSentenceStart =
+            "Within this region they make up a large percentage of the population in the "
+        break
+      case 2:
+        pageRaceRegions.textContent = `${regionSentenceStart} ${
+          race.geography.regions[0]
+        } and ${race.geography.regions[1]} regions.`
+        citiesSentenceStart =
+            "Within these regions they make up a large percentage of the population in the "
+        break
+      case 3:
+        pageRaceRegions.textContent = `${regionSentenceStart} ${
+          race.geography.regions[0]
+        }, ${race.geography.regions[1]}, and ${
+          race.geography.regions[2]
+        } regions.`
+        citiesSentenceStart =
+            "Within these regions they make up a large percentage of the population in the "
+        break
+      default:
       }
       switch (race.geography.cities.length) {
-        case 1:
-          pageRaceCities.textContent = `${citiesSentenceStart} city of ${race.geography.cities[0]}`
-          break;
-        case 2:
-          pageRaceCities.textContent = `${citiesSentenceStart} cities of ${race.geography.cities[0]} and ${race.geography.cities[1]}`
-          break
-        case 3:
-          pageRaceCities.textContent = `${citiesSentenceStart} cities of ${race.geography.cities[0]}, ${race.geography.cities[1]}, and ${race.geography.cities[2]}`
-          break
-        case 4:
-          pageRaceCities.textContent = `${citiesSentenceStart} cities of ${race.geography.cities[0]}, ${race.geography.cities[1]}, ${race.geography.cities[2]}, and ${race.geography.cities[3]}`
-          break
-        case 5:
-          pageRaceCities.textContent = `${citiesSentenceStart} cities of ${race.geography.cities[0]}, ${race.geography.cities[1]}, ${race.geography.cities[2]}, ${race.geography.cities[3]}, and ${race.geography.cities[4]}`
-          break
-        default:
+      case 1:
+        pageRaceCities.textContent = `${citiesSentenceStart} city of ${
+          race.geography.cities[0]
+        }`
+        break
+      case 2:
+        pageRaceCities.textContent = `${citiesSentenceStart} cities of ${
+          race.geography.cities[0]
+        } and ${race.geography.cities[1]}`
+        break
+      case 3:
+        pageRaceCities.textContent = `${citiesSentenceStart} cities of ${
+          race.geography.cities[0]
+        }, ${race.geography.cities[1]}, and ${race.geography.cities[2]}`
+        break
+      case 4:
+        pageRaceCities.textContent = `${citiesSentenceStart} cities of ${
+          race.geography.cities[0]
+        }, ${race.geography.cities[1]}, ${race.geography.cities[2]}, and ${
+          race.geography.cities[3]
+        }`
+        break
+      case 5:
+        pageRaceCities.textContent = `${citiesSentenceStart} cities of ${
+          race.geography.cities[0]
+        }, ${race.geography.cities[1]}, ${race.geography.cities[2]}, ${
+          race.geography.cities[3]
+        }, and ${race.geography.cities[4]}`
+        break
+      default:
       }
     }
   })
@@ -58,26 +84,27 @@ document.querySelector("form").addEventListener("submit", event => {
   event.preventDefault()
   let formObj = new FormData(event.target)
   let sendableRace = {
-    "name": formObj.get("name"),
-    "description": formObj.get("description"),
-    "geography": {
-      "regions": formObj.get("regions").split(", "),
-      "cities": formObj.get("cities").split(", ")
+    name: formObj.get("name"),
+    description: formObj.get("description"),
+    geography: {
+      regions: formObj.get("regions").split(", "),
+      cities: formObj.get("cities").split(", ")
     }
   }
   postNewRace(sendableRace)
 })
 
-function postNewRace(race){
+function postNewRace(race) {
   fetch(APIurl, {
     method: "POST",
     body: JSON.stringify(race),
-    headers: new Headers({"Content-Type": "application/json"})
+    headers: new Headers({ "Content-Type": "application/json" })
   })
     .then(resp => resp.json())
     .then(resp => {
-      console.log(resp)
-      $pAddedRace.textContent = `Congratulations you successfully added ${resp[resp.length - 1].name} to the world of Ahnvaer`
+      $pAddedRace.textContent = `Congratulations you successfully added ${
+        resp[resp.length - 1].name
+      } to the world of Ahnvaer`
       return resp
     })
 }
